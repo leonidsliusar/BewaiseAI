2a8d963aabc4796fa2ae50db9e0388b5960ff7b7
Leonid Sliusar <loslyusar@gmail.com>
2023-10-12 23:36:17 +0300

    readme

--- tests/test_dals.py
+++ tests/test_dals.py
@@ -2,6 +2,6 @@ from app.core.dals import DBManager
 
 
 async def test_create(setup_and_teardown_db, question):
-    # question.serializeDB.obj)
-    assert await DBManager().create(question, setup_and_teardown_db) == []
+    obj = await DBManager().create(question, setup_and_teardown_db)
+    assert [item.model_dump() for item in obj] == [item.model_dump() for item in question.serializeDB.obj]
     assert await DBManager().create(question, setup_and_teardown_db) == []
